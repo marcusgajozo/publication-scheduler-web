9fa71f96e87e426dcd8dcca24d55c52630a584b8
Marcus Gajozo <marcusgajozo@gmail.com>
2026-01-27 16:45:14 -0400

    chore: adjust variables

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,3 @@
-export const DATABASE_PRISMA_URL = process.env.DATABASE_PRISMA_URL;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const NODE_ENV = process.env.NODE_ENV || "development";
-export const NEXTAUTH_URL = process.env.NEXTAUTH_URL || "http://localhost:3000";
-export const PUBLICATION_SCHEDULER_API_URL =
-  process.env.PUBLICATION_SCHEDULER_API_URL || "http://localhost:3000/api/v1";
+export const NODE_ENV = process.env.NODE_ENV;
+export const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
+export const PUBLICATION_SCHEDULER_API_URL = process.env.PUBLICATION_SCHEDULER_API_URL;
