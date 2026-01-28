0976845d734ff39651d9cf3ebe7e4705a2fe3f3d
Marcus Gajozo <marcusgajozo@gmail.com>
2026-01-28 18:17:57 -0400

    test(cypress): config cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,30 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      login(): Chainable<void>;
+    }
+  }
+}
+
+export {};
+
+Cypress.Commands.add("login", () => {
+  const USER_EMAIL = Cypress.env("USER_EMAIL");
+  const USER_PASSWORD = Cypress.env("USER_PASSWORD");
+
+  if (!USER_EMAIL || !USER_PASSWORD) {
+    throw new Error("USER_EMAIL or USER_PASSWORD not defined");
+  }
+
+  cy.session("nextauth-session", () => {
+    cy.visit("/sign-in");
+    cy.get('input[name="email"]').type(USER_EMAIL);
+    cy.get('input[name="password"]').type(USER_PASSWORD);
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should("not.include", "/sign-in");
+  });
+});
